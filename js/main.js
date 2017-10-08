84445bda1660a38b173c6e011d7426f2cdbe61d6
Bibesh Manandhar <bibesh.manandhar@gmail.com>
2017-10-08 09:30:53 +0545

    Update JS

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
-$.ajax({
-  url:"https://api.github.com/users/sayami007",
-  statusCode:{
-    200:function(){
-      console.log("done")
-    },
-    404:function(){
-      console.log("Page not found")
-    }
-  }
-}).then(function(data){
-    $('#username').append(data.login)
-    $('#name').append(data.name)
-    $('#image').attr('src',data.avatar_url)
+$(document).ready(function(){
+  $('#showWeather').on('click',function(){
+    var name = $('#username').val();
+     $.ajax({
+      url:"https://api.github.com/users/"+name,
+      statusCode:{
+        200:function(data){
+          $('#name').text(data.name)
+          $('#image').attr('src',data.avatar_url)
+        },
+        404:function(){
+
+          $('#name').text("N/A")
+          $('#image').attr('alt',"N/A")
+          $('#image').attr('src',"N/A")
+          alert("Username not found")
+        }
+      }
+    })
+  });
 });
