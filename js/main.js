12f7eae8353cb8947141e337b9a426b0ab48576b
Bibesh Manandhar <bibesh.manandhar@gmail.com>
2017-10-08 14:16:11 +0545

    Weather

--- js/main.js
+++ js/main.js
@@ -1,21 +1,19 @@
 $(document).ready(function(){
-  $('#showWeather').on('click',function(){
-    var name = $('#username').val();
-     $.ajax({
-      url:"https://api.github.com/users/"+name,
-      statusCode:{
-        200:function(data){
-          $('#name').text(data.name)
-          $('#image').attr('src',data.avatar_url)
-        },
-        404:function(){
-
-          $('#name').text("N/A")
-          $('#image').attr('alt',"N/A")
-          $('#image').attr('src',"N/A")
-          alert("Username not found")
-        }
-      }
-    })
-  });
+  	$('#showWeather').on('click',function(){
+    	var location = $('#location').val();
+    	$.ajax({
+    		url:'http://api.openweathermap.org/data/2.5/weather?q='+location+'&appid=4887f9ed6473e74aa7f23760dab7f288&units=metric',
+    		success:function(data){
+    			console.log(data.main.temp)
+    			$('#temparature').text(data.main.temp)
+    			$('#locationValue').text(data.name)
+    		},
+    		error:function(response){
+    			alert('No Address found')
+    		},
+    		complete:function(response){
+				console.log(response)
+			}
+   		})
+	})
 });
